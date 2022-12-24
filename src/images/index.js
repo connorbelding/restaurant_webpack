e19fdeee9d9655a407e99c8df0c0da9b5d231f52
connorbelding <the_connn@hotmail.com>
2022-12-23 23:19:15 -0500

    Added index file for easier importing

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+import BurgerImg from "./burger.jpg";
+import IceCreamImg from "./icecream.jpg";
+import PastaImg from "./pasta.jpg";
+import PizzaImg from "./pizza.jpg";
+import SteakImg from "./steak.jpg";
+import WingsImg from "./wings.jpg";
+
+export { BurgerImg, IceCreamImg, PastaImg, PizzaImg, SteakImg, WingsImg };
