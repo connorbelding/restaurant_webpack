78baecf4b4e8180b20f3adefd790e17083e459c2
connorbelding <the_connn@hotmail.com>
2022-12-23 23:17:19 -0500

    Testing file loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("test2");
+import menuItems from "./menu_items";
+import { generateElement } from "./utils";
+
+const root = document.getElementById("root");
+
+menuItems.forEach((menuItem) => {
+  const title = generateElement({
+    name: "h2",
+    properties: { textContent: menuItem.name },
+  });
+  const desc = generateElement({
+    name: "p",
+    properties: { textContent: menuItem.desc },
+  });
+  const img = generateElement({
+    name: "img",
+    properties: {
+      src: menuItem.imgUrl,
+      alt: "food item",
+    },
+  });
+
+  const wrapper = generateElement({ name: "div" });
+
+  wrapper.appendChild(title);
+  wrapper.appendChild(desc);
+  wrapper.appendChild(img);
+
+  root.appendChild(wrapper);
+});
