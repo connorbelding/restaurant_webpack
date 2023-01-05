01f7d89d4eb4a0337eed3726369e7feea6309e4b
connorbelding <the_connn@hotmail.com>
2023-01-04 22:41:03 -0500

    Renamed menu items file

--- src/menu-items.js
+++ src/menu-items.js
@@ -10,33 +10,39 @@ import {
 const menuItems = [
   {
     name: "Chicken Wings",
-    desc: "A basket of wings and dipping sauce of your choice",
+    desc: "Served with dipping sauce and celery sticks",
     imgUrl: WingsImg,
+    price: 11.99,
   },
   {
     name: "Hamburger",
     desc: "Served on a toasted bun with french fries",
     imgUrl: BurgerImg,
+    price: 15.99,
   },
   {
     name: "Pasta",
-    desc: "Served with marinara or alfredo sauce and a salad",
+    desc: "Served with breadsticks and salad",
     imgUrl: PastaImg,
+    price: 14.99,
   },
   {
     name: "Ice Cream",
     desc: "8 different flavors served in a cone or cup",
     imgUrl: IceCreamImg,
+    price: 6.99,
   },
   {
     name: "Pizza",
-    desc: "Thick crust pizza served with a variety of toppings",
+    desc: "Thick-crust pizza served with a variety of toppings",
     imgUrl: PizzaImg,
+    price: 14.99,
   },
   {
     name: "Steak",
-    desc: "Served with mashed potatoes and corn",
+    desc: "Served with mashed potatoes",
     imgUrl: SteakImg,
+    price: 18.99,
   },
 ];
 
