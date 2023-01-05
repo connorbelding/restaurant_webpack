bffd1e5630bd4fea5399ba9af620a2f55de771ea
connorbelding <the_connn@hotmail.com>
2023-01-04 23:54:53 -0500

    Resized images

--- src/images/index.js
+++ src/images/index.js
@@ -4,5 +4,18 @@ import PastaImg from "./pasta.jpg";
 import PizzaImg from "./pizza.jpg";
 import SteakImg from "./steak.jpg";
 import WingsImg from "./wings.jpg";
+import BackgroundImg from "./bg.jpg";
+import MenuIconImg from "./menu-icon.png";
+import CloseIconImg from "./close-icon.png";
 
-export { BurgerImg, IceCreamImg, PastaImg, PizzaImg, SteakImg, WingsImg };
+export {
+  BurgerImg,
+  IceCreamImg,
+  PastaImg,
+  PizzaImg,
+  SteakImg,
+  WingsImg,
+  BackgroundImg,
+  MenuIconImg,
+  CloseIconImg,
+};
